89dd9a499fc52de9acdd04c1c785893b3d046b96
pylypchukroman <romanpylypchuk@gmail.com>
2024-01-15 20:22:24 +0100

    added connect to remote db

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const app = require('./app')
+const mongoose = require('mongoose').default;
+const app = require('./app');
+const DB_HOST = "mongodb+srv://Roman:enterme@cluster0.szr8x2o.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(3000);
+  console.log("Database connection successful");
+}).catch(error => {
+  console.log(error.message);
+  process.exit(1);
 })

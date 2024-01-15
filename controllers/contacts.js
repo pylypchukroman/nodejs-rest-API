2945b914898d7c05c6ead995b02299134947b075
pylypchukroman <romanpylypchuk@gmail.com>
2024-01-15 11:44:20 +0100

    added decorators

---
+++ controllers/contacts.js
@@ -0,0 +1,50 @@
+const contacts = require("../models/contacts");
+const { HttpError, ctrlWrapper} = require("../helpers");
+
+const getAll = async (req, res) => {
+    const result = await contacts.listContacts();
+    res.json(result);
+};
+
+const getById = async (req, res) => {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+        throw HttpError(404, "Not Found");
+    }
+    res.json(result);
+};
+
+const addContact = async (req, res) => {
+
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+};
+
+const deleteContact = async (req, res) => {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+        throw HttpError(404, "Not Found");
+    }
+    res.json({
+        message: "contact deleted"
+    })
+};
+
+const editContact = async (req, res) => {
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+        throw HttpError(404, "Not Found");
+    }
+    res.json(result);
+};
+
+module.exports = {
+    getAll: ctrlWrapper(getAll),
+    getById: ctrlWrapper(getById),
+    addContact: ctrlWrapper(addContact),
+    deleteContact: ctrlWrapper(deleteContact),
+    editContact: ctrlWrapper(editContact)
+};
